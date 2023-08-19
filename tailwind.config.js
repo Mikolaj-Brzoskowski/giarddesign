ed7bf8b61715fd84130b5e67110daf139cb308a4
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-08-19 14:55:53 +0200

    added flowbite dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}",
+  "./node_modules/flowbite/**/*.js"
+],
   theme: {
     extend: {
       colors: {
@@ -14,6 +16,6 @@ module.exports = {
       'montserrat': ['Montserrat', 'serif'],
     }
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
