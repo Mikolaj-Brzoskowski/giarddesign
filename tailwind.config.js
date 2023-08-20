f4254ba6e10e6310fa7f5d7d255e516ac551712e
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-08-21 01:47:51 +0200

    added images mask and more button

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,15 @@ module.exports = {
         'green': '#1B5B31',
         'grey': '#F5F0EC',
       },
+      scale: {
+        '85': '0.85',
+      }
     },
     fontFamily: {
       'inter': ['Inter', 'serif'], 
       'montserrat': ['Montserrat', 'serif'],
     }
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [require('flowbite/plugin'), require("tailwind-gradient-mask-image")],
 }
 
