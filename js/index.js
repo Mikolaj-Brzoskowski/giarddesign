37dc4b0a9888ad1e393fb4fd61c9ac2f6e34518a
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-08-22 14:08:43 +0200

    limited images size/resolution for optimalisation

--- js/index.js
+++ js/index.js
@@ -11,4 +11,8 @@ window.onclick = function(event) {
     if (event.target == popup) {
         popup.style.display = "none";
     }
+    if (event.target == gallery) {
+        gallery.style.display = "none";
+        document.body.style.overflow = "auto"
+    }
 }
